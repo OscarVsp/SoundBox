8969f2470ae192bce7591bc3bd25363ac0145cef
oscarvsp <boscar.vs98@gmail.com>
2023-02-19 22:46:08 +0100

    added volume and replay

--- cogs/soundbox/music.py
+++ cogs/soundbox/music.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-import asyncio
 from typing import List
 
 import disnake
 
 
 class Music:
-    def __init__(self, name: str, emote: str, volume: float = 1.0) -> None:
+    def __init__(self, name: str, emote: str, volume: float = 1.0, description: str = None) -> None:
         self.file_name = f"cogs/soundbox/sounds/{name}.wav"
         self.name = name
+        self.description = description
         self.volume = volume
         self.emote: str = emote
 
-    async def play(self, voice: disnake.VoiceClient):
-        voice.play(
-            disnake.PCMVolumeTransformer(disnake.FFmpegPCMAudio(self.file_name)),
-            after=lambda e: print(f"Player error: {e}") if e else None,
-        )
-        while voice.is_playing():
-            await asyncio.sleep(0.1)
-        return
+    @property
+    def sound(self) -> disnake.PCMVolumeTransformer:
+        sound = disnake.PCMVolumeTransformer(disnake.FFmpegPCMAudio(self.file_name))
+        sound.volume = self.volume
+        return sound
 
 
-music_list: List[Music] = [Music("On les souleve", "<:BoudhaPog:854084071055032340>"), Music("Fontaine", "⛲")]
+music_list: List[Music] = [
+    Music("On les souleve", "<:BoudhaPog:854084071055032340>", description="Ace D Ashura durant une finale de Clash"),
+    Music("Fontaine", "⛲", description="Ace D Ashura en trolling"),
+]
